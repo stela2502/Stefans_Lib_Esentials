91c4438d3c2990951fc09d9028e25f8ffc7d1316
Stefan Lang <Stefan.Lang@med.lu.se>
2016-06-02 08:40:07 +0200

    Changed the structure of the written script files to include a LICENCE head1 entry.

--- Stefans_Libs_Essentials/bin/binCreate.pl
+++ Stefans_Libs_Essentials/bin/binCreate.pl
@@ -141,20 +141,22 @@ foreach my $variableStr (@commandLineSwitches) {
 
 my $string = "#! /usr/bin/perl -w
 
-#  Copyright (C) " . root::Today() . " Stefan Lang
+=head1 LICENCE
 
-#  This program is free software; you can redistribute it 
-#  and/or modify it under the terms of the GNU General Public License 
-#  as published by the Free Software Foundation; 
-#  either version 3 of the License, or (at your option) any later version.
+  Copyright (C) " . root::Today() . " Stefan Lang
 
-#  This program is distributed in the hope that it will be useful, 
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of 
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-#  See the GNU General Public License for more details.
+  This program is free software; you can redistribute it 
+  and/or modify it under the terms of the GNU General Public License 
+  as published by the Free Software Foundation; 
+  either version 3 of the License, or (at your option) any later version.
 
-#  You should have received a copy of the GNU General Public License 
-#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 =head1  SYNOPSIS
 
