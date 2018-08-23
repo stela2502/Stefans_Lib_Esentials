c35cfea822cac3435c5821897ec3976372a89673
stefanlang <Stefan.Lang@med.lu.se>
2018-08-23 15:49:49 +0200

    re-activate plotting

--- Stefans_Libs_Essentials/lib/stefans_libs/plot/simpleBarGraph.pm
+++ Stefans_Libs_Essentials/lib/stefans_libs/plot/simpleBarGraph.pm
@@ -15,7 +15,7 @@ package simpleBarGraph;
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-#use stefans_libs::plot::figure;
+use stefans_libs::plot::figure;
 use base ('figure');
 use strict;
 
